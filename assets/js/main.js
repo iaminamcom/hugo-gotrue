c88b52a348c0ba6dba51344ccb8588de76b91082
minaminfo <34743206+minamme@users.noreply.github.com>
2022-06-29 19:14:00 +0500

    added leflet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,27 @@
-import GoTrue from 'gotrue-js';
+const center = L.latLng(51.505, -0.09);
+const zoom = 15;
 
-const auth = new GoTrue({
-  APIUrl: 'https://transcendent-pegasus-7ff91f.netlify.app/.netlify/identity',
-  audience: '',
-  setCookie: false,
-});
+const tiles = L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  { maxZoom: 17, minZoom: zoom, attribution: "© OpenStreetMap" }
+);
+const map = L.map("map", { center, zoom, layers: [tiles] });
+const markers = L.markerClusterGroup();
 
-// auth
-//   .login('inamkbmail@gmail.com', 'password', true)
-//   .then((r) => console.log(`Success! Response: ${{ r }}`))
-//   .catch((error) => console.log(`Failed :( ${error}`));
-auth.confirm(location.href.split('=')[1], true)
-  .then((r) => console.log(r)).catch((err) => console.log(err));
+const addressPoints = [[-0.09, 51.505], [-0.09, 51.505], [-0.091, 51.505]];
+
+for (let i = 0; i < addressPoints.length; i++) {
+  const a = addressPoints[i];
+  const marker = L.marker(new L.LatLng(a[1], a[0]), { title: "title" });
+  marker.bindPopup("title");
+  markers.addLayer(marker);
+}
+
+map.addLayer(markers);
+map.on("moveend", addMarkers);
+
+function addMarkers(e) {
+  const { _northEast, _southWest } = e.target.getBounds();
+  console.log(_northEast, _southWest);
+}
+// db.data.find({"location.geometry": { $geoWithin: { $box: [[-0.08836805820465088, 51.50545363381515], [-0.09179592132568361, 51.5044953223804]]} }})
