1516597416a1c234a64cb24924b2dd812b3ddba3
minaminfo <34743206+minamme@users.noreply.github.com>
2022-06-30 11:22:48 +0500

    xxxxxx

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,40 +7,34 @@ const auth = new GoTrue({
   audience: '',
   setCookie: false,
 });
-// auth.login('inamkbmail@gmail.com', 'passw0rd', true)
-//   .then((r) => console.log(r)).catch((e) => console.log(e));
+// auth.login('inamsonyx@gmail.com', 'passw0rd', true).then((r) => console.log(r)).catch((e) => console.log(e));
 
-auth
-  .confirm('WF49TPhiwGGcKn-Vt4v3Rg', 'passw0rd', true)
-  .then((response) => {
-    console.log('Confirmation email sent', JSON.stringify({ response }));
-  })
-  .catch((error) => console.log(error));
+// auth.confirm('TL0VSybEttvrBL6oN2cvBA', 'passw0rd', true).then((r) => console.dir({ r })).catch((e) => console.log(e));
 
-const center = L.latLng(51.505, -0.09);
-const zoom = 15;
+// const center = L.latLng(51.505, -0.09);
+// const zoom = 15;
 
-const tiles = L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  { maxZoom: 17, minZoom: zoom, attribution: "© OpenStreetMap" }
-);
-const map = L.map("map", { center, zoom, layers: [tiles] });
-const markers = L.markerClusterGroup();
+// const tiles = L.tileLayer(
+//   "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+//   { maxZoom: 17, minZoom: zoom, attribution: "© OpenStreetMap" }
+// );
+// const map = L.map("map", { center, zoom, layers: [tiles] });
+// const markers = L.markerClusterGroup();
 
-const addressPoints = [[-0.09, 51.505], [-0.09, 51.505], [-0.091, 51.505]];
+// const addressPoints = [[-0.09, 51.505], [-0.09, 51.505], [-0.091, 51.505]];
 
-for (let i = 0; i < addressPoints.length; i++) {
-  const a = addressPoints[i];
-  const marker = L.marker(new L.LatLng(a[1], a[0]), { title: "title" });
-  marker.bindPopup("title");
-  markers.addLayer(marker);
-}
+// for (let i = 0; i < addressPoints.length; i++) {
+//   const a = addressPoints[i];
+//   const marker = L.marker(new L.LatLng(a[1], a[0]), { title: "title" });
+//   marker.bindPopup("title");
+//   markers.addLayer(marker);
+// }
 
-map.addLayer(markers);
-map.on("moveend", addMarkers);
+// map.addLayer(markers);
+// map.on("moveend", addMarkers);
 
-function addMarkers(e) {
-  const { _northEast, _southWest } = e.target.getBounds();
-  console.log(_northEast, _southWest);
-}
+// function addMarkers(e) {
+//   const { _northEast, _southWest } = e.target.getBounds();
+//   console.log(_northEast, _southWest);
+// }
 // db.data.find({"location.geometry": { $geoWithin: { $box: [[-0.08836805820465088, 51.50545363381515], [-0.09179592132568361, 51.5044953223804]]} }})
