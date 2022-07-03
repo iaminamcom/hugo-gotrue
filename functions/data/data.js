b0738df10cd7ac0435c8d61b3488027baa7f4686
minaminfo <34743206+minamme@users.noreply.github.com>
2022-07-03 15:06:47 +0500

    fix

--- functions/data/data.js
+++ functions/data/data.js
@@ -1,3 +1,5 @@
+// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
+
 const fetch = require('node-fetch')
 
 const handler = async function (event, context) {
@@ -14,6 +16,7 @@ const handler = async function (event, context) {
   try {
     const response = await fetch('https://api.chucknorris.io/jokes/random')
     if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
       return { statusCode: response.status, body: response.statusText }
     }
     const data = await response.json()
@@ -23,9 +26,11 @@ const handler = async function (event, context) {
       body: JSON.stringify({ identity, user, msg: data.value }),
     }
   } catch (error) {
+    // output to netlify function log
     console.log(error)
     return {
       statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
       body: JSON.stringify({ msg: error.message }),
     }
   }
